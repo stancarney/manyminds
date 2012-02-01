079a4fd60a5c52afe8552d2ae0dce1dc5177f6cc
Stan Carney <stan.carney@rootsh.me>
2012-01-31 20:08:59 -0700

    More work on getting things sorted.
    --HG--
    rename : index.html => views/chat.html

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var express = require('express');
+var app = express.createServer()
+		, io = require('socket.io').listen(app);
 
 app.listen(8080);
 
+app.use(express.cookieParser());
+app.use(express.session({ secret: "keyboard cat" }));
+app.use(express.bodyParser());
+
+app.set('view options', {
+    open: '{{',
+    close: '}}',
+	layout: false
+});
+
+app.register('.html', require('ejs'));
+
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-	console.log('Serving file');
+	res.sendfile(__dirname + '/views/index.html');
+});
+
+app.post('/chat', function (req, res) {
+	req.session.user = req.body.nickname
+	res.redirect('/chat')
+});
+
+app.get('/chat', function (req, res) {
+	res.render(__dirname + '/views/chat.html', { user: req.session.user });
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.on('set nickname', function (name) {
-    socket.set('nickname', name, function () {
-      socket.emit('ready');
-	    console.log('Set nickname ', name);
-    });
-  });
 
-  socket.on('messsage', function () {
-    socket.get('value', function (err, value) {
-      console.log('Chat message by ', value);
-    });
-  });
+	socket.on('set nickname', function (name) {
+		socket.set('nickname', name, function () {
+			socket.emit('remove user', { name: name, id: socket.id });
+			socket.broadcast.emit('remove user', { name: name, id: socket.id });
+			socket.emit('add user', { name: name, id: socket.id });
+			socket.broadcast.emit('add user', { name: name, id: socket.id });
+		});
+	});
+
+	socket.on('message', function (message) {
+		socket.get('nickname', function (err, name) {
+			socket.emit('chat', { user: name, value: message.value});
+			socket.broadcast.emit('chat', { user: name, value: message.value});
+		});
+	});
+
+	socket.on('typing', function () {
+		socket.get('nickname', function (err, name) {
+			socket.broadcast.emit('typing', { id: socket.id });
+		});
+	});
+
+	socket.on('adduser', function (user) {
+		console.log('Using is typing a message');
+		socket.emit('ready', {});
+	});
+
+	socket.on('deluser', function (user) {
+		console.log('Using is typing a message');
+		socket.emit('ready', {});
+	});
 });
 
